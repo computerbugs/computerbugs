2520efec4c8dc33cbfe32c878d682d60bb052b23
常稼笙 <changjiasheng@126.com>
2024-03-04 17:43:45 +0800

    init

---
+++ src/main/java/com/example/changtest/ChangtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.changtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/changtest/ChangtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.changtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
