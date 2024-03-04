247cb3aaa2b9dd97e67df4ed7d842e24114e9af0
常稼笙 <changjiasheng@126.com>
2024-03-04 18:12:40 +0800

    modify

---
+++ src/main/java/com/example/changtest/OtsDateUtils.java
@@ -0,0 +1,31 @@
+package com.example.changtest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 日期处理工具类
+ *
+ * @author changjiasheng
+ * @date 2024/3/4
+ **/
+public class OtsDateUtils {
+
+    private static final String dateRegx = "^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$";
+
+    public static String Time2GMT(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss 'GMT'", Locale.US);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return sdf.format(date);
+    }
+
+    public static boolean isDate(String dateStr) {
+        Pattern pattern = Pattern.compile(dateRegx);
+        Matcher matcher = pattern.matcher(dateStr);
+        return matcher.matches();
+    }
+}
