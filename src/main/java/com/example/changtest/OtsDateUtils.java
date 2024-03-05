1cc33894a750c211c64383a019a92e16d78585f4
常稼笙 <changjiasheng@126.com>
2024-03-05 15:47:32 +0800

    解决冲突

--- src/main/java/com/example/changtest/OtsDateUtils.java
+++ src/main/java/com/example/changtest/OtsDateUtils.java
@@ -2,13 +2,14 @@ package com.example.changtest;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.cars.ict.otsws.service.exception.SystemOtsException;
+
 
 /**
  * 日期处理工具类
@@ -33,18 +34,53 @@ public class OtsDateUtils {
         return matcher.matches();
     }
 
-    public static String covertDateStrFormat(String dateStr, String srcFormat, String descFormat) {
+    public static String covertDateStrFormat(String dateStr, String srcFormat, String descFormat) throws Exception {
         SimpleDateFormat format_src = new SimpleDateFormat(srcFormat);
         SimpleDateFormat format_desc = new SimpleDateFormat(descFormat);
         try {
             Date date = format_src.parse(dateStr);
             dateStr = format_desc.format(date);
         } catch (ParseException e) {
-            log.error("could not convert the dateStr [" + dateStr + "] from formatter [" + srcFormat
-                + "] to the given formatter[" + descFormat + "]", e);
-            throw new SystemOtsException(e);
+            throw new Exception(e);
         }
         return dateStr;
+    }
+
+    public static String getWeekByDate(Date date) {
+        String weekofDay = "";
+        try {
+            Calendar now = Calendar.getInstance();
+            now.setTime(date);
+            int w = now.get(Calendar.DAY_OF_WEEK);
+
+            switch (w) {
+                case 1:
+                    weekofDay = "周日";
+                    break;
+                case 2:
+                    weekofDay = "周一";
+                    break;
+                case 3:
+                    weekofDay = "周二";
+                    break;
+                case 4:
+                    weekofDay = "周三";
+                    break;
+                case 5:
+                    weekofDay = "周四";
+                    break;
+                case 6:
+                    weekofDay = "周五";
+                    break;
+                case 7:
+                    weekofDay = "周六";
+                    break;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return weekofDay;
     }
 
 }
