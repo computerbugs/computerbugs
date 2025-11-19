27ce9dd23ef665716fc435d93315ad6538082726
computerbugs <7516850+computerbugs@user.noreply.gitee.com>
2025-11-19 11:20:10 +0800

    测试版本

--- src/main/java/com/example/changtest/FileToString122.java
+++ src/main/java/com/example/changtest/FileToString122.java
@@ -9,11 +9,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileToString {
+public class FileToString122 {
 
     public static void main(String[] args) {
         // 文件路径（替换为你的文件路径）
-        String filePath = "test1111.txt";
+        String filePath = "test.txt";
         // 用于拼接文件内容的字符串缓冲区（效率高于 String 直接拼接）
         StringBuilder content = new StringBuilder();
 
@@ -24,16 +24,10 @@ public class FileToString {
             while ((line = br.readLine()) != null) {
                 // 拼接当前行内容，若需要保留换行符可加 "\n"
                 content.append(line).append("\n");
-                content.append(line).append("\n");
-                content.append(line).append("\n");
-
-                content.append(line).append("\n");
             }
         } catch (IOException e) {
             // 处理文件读取异常（如文件不存在、权限问题等）
             e.printStackTrace();
-            System.out.println("文件内容：");
-
         }
 
         // 转换为字符串并输出
